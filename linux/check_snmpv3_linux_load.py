f8774a1b56e0f64456ca8f632549cc72db48bd49
hvad <david.hannequin@gmail.com>
2019-11-17 20:22:31 +0100

    add timeout for snmp plugins for load on Linux.

--- linux/check_snmpv3_linux_load.py
+++ linux/check_snmpv3_linux_load.py
@@ -24,27 +24,29 @@
 from pysnmp.hlapi import *
 import argparse
 
+oid_load1='1.3.6.1.4.1.2021.10.1.3.1'
+oid_load5='1.3.6.1.4.1.2021.10.1.3.2'
+oid_load15='1.3.6.1.4.1.2021.10.1.3.3'
+
 def parse_args():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-H', '--host', default='127.0.0.1', type=str, help='hostname or ip address')
     parser.add_argument('-p', '--port', default='161', type=int, help='port')
-#    parser.add_argument('-C', '--community', default='public', type=str, help='snmp community')
     parser.add_argument('-u', '--username', default='nagios', type=str, help='login for snmpv3')
     parser.add_argument('-x', '--auth_pass', default='password', type=str, help='auth password for snmpv3')
-#    parser.add_argument('-X', '--auth_protocol', default='SHA', type=str, help='auth protocol for snmpv3')
     parser.add_argument('-a', '--priv_pass', default='private', type=str, help='private password for snmpv3')
-#    parser.add_argument('-A', '--priv_protocol', default='AES', type=str, help='private protocol for snmpv3')
     parser.add_argument('-w', '--warning', default='3,2,1')
     parser.add_argument('-c', '--critical', default='4,3,2')
+    parser.add_argument('-t', '--timeout', default='15')
+
     args = parser.parse_args()
     host = args.host
     port = args.port
     username = args.username
     auth_pass = args.auth_pass
-#    auth_protocol = args.auth_protocol
     priv_pass = args.priv_pass
- #   priv_protocol = args.priv_protocol
+    timeout = int(args.timeout)
 
     critical = map(int, args.critical.split(','))
     warning = map(int, args.warning.split(','))
@@ -52,24 +54,22 @@ def parse_args():
     (cload1, cload5, cload15) = critical
     (wload1, wload5, wload15) = warning
     
-    return host,port,username,auth_pass,priv_pass,cload1,cload5,cload15,wload1,wload5,wload15
+    return host,port,username,auth_pass,priv_pass,timeout,cload1,cload5,cload15,wload1,wload5,wload15
 
-
-#def main():
 def get_data():
 
-    host,port,username,auth_pass,priv_pass,cload1,cload5,cload15,wload1,wload5,wload15 = parse_args()
+    host,port,username,auth_pass,priv_pass,timeout,cload1,cload5,cload15,wload1,wload5,wload15 = parse_args()
     
     errorIndication, errorStatus, errorIndex, varBinds = next(
         getCmd(SnmpEngine(),
                UsmUserData(username, auth_pass, priv_pass,
                            authProtocol=usmHMACSHAAuthProtocol,
                            privProtocol=usmAesCfb128Protocol),
-               UdpTransportTarget((host, port)),
+               UdpTransportTarget((host, port), timeout=timeout),
                ContextData(),
-               ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.1')),
-               ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.2')),
-               ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.3')))
+               ObjectType(ObjectIdentity(oid_load1)),
+               ObjectType(ObjectIdentity(oid_load5)),
+               ObjectType(ObjectIdentity(oid_load15)))
      )
     
     if errorIndication:
@@ -94,7 +94,7 @@ def get_data():
 
 def main():
 
-    host,port,username,auth_pass,priv_pass,cload1,cload5,cload15,wload1,wload5,wload15 = parse_args()
+    host,port,username,auth_pass,priv_pass,timeout,cload1,cload5,cload15,wload1,wload5,wload15 = parse_args()
    
     load1,load5,load15 = get_data()
 
