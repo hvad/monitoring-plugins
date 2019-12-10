dcc175ea0467fd208a01a9381e04f24ed278ac16
hvad <david.hannequin@gmail.com>
2019-12-10 21:28:57 +0100

    factorization.

--- linux/check_snmpv3_linux_memory.py
+++ linux/check_snmpv3_linux_memory.py
@@ -6,7 +6,7 @@
 #   Date: 2019-08-08
 #   URL: https://github.com/hvad/monitoring-plugins
 #   
-#   Plugins to check linux load by SNMP.
+#   Plugins to check linux memory and swap by SNMP.
 #
 # Monitoring plugin is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,45 +24,35 @@
 from pysnmp.hlapi import *
 import argparse
 
-oid_ram='1.3.6.1.4.1.2021.4.5.0'
-oid_ram_used='1.3.6.1.4.1.2021.4.6.0'
-oid_swap='1.3.6.1.4.1.2021.4.3.0'
-oid_swap_used='1.3.6.1.4.1.2021.4.4.0'
-
 def parse_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-H', '--host', default='127.0.0.1', type=str, help='hostname or ip address')
+    parser.add_argument('-H', '--host', required=True, type=str, help='hostname or ip address')
     parser.add_argument('-p', '--port', default='161', type=int, help='port')
-    parser.add_argument('-u', '--username', default='nagios', type=str, help='login for snmpv3')
-    parser.add_argument('-x', '--auth_pass', default='password', type=str, help='auth password for snmpv3')
-    parser.add_argument('-a', '--priv_pass', default='private', type=str, help='private password for snmpv3')
+    parser.add_argument('-u', '--username', required=True, type=str, help='login for snmpv3')
+    parser.add_argument('-x', '--auth_pass', required=True, type=str, help='auth password for snmpv3')
+    parser.add_argument('-a', '--priv_pass', required=True, type=str, help='private password for snmpv3')
     parser.add_argument('-w', '--warning', default='80', type=int, help='Warning thresold')
     parser.add_argument('-c', '--critical', default='90', type=int, help='Critical thresold')
     args = parser.parse_args()
-    host = args.host
-    port = args.port
-    username = args.username
-    auth_pass = args.auth_pass
-    priv_pass = args.priv_pass
-    warning = args.warning
-    critical = args.critical
 
-#    critical = map(int, args.critical.split(','))
-#    warning = map(int, args.warning.split(','))
+    return args
 
-    return host,port,username,auth_pass,priv_pass,warning,critical
+def data():
 
-def get_data():
+    args = parse_args()
 
-    host,port,username,auth_pass,priv_pass,warning,critical = parse_args()
+    oid_ram='1.3.6.1.4.1.2021.4.5.0'
+    oid_ram_used='1.3.6.1.4.1.2021.4.6.0'
+    oid_swap='1.3.6.1.4.1.2021.4.3.0'
+    oid_swap_used='1.3.6.1.4.1.2021.4.4.0'
     
     errorIndication, errorStatus, errorIndex, varBinds = next(
         getCmd(SnmpEngine(),
-               UsmUserData(username, auth_pass, priv_pass,
+               UsmUserData(args.username, args.auth_pass, args.priv_pass,
                            authProtocol=usmHMACSHAAuthProtocol,
                            privProtocol=usmAesCfb128Protocol),
-               UdpTransportTarget((host, port)),
+               UdpTransportTarget((args.host, args.port)),
                ContextData(),
                ObjectType(ObjectIdentity(oid_ram)),
                ObjectType(ObjectIdentity(oid_ram_used)),
@@ -76,34 +66,26 @@ def get_data():
         print('%s at %s' % (errorStatus.prettyPrint(),
                             errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
     else:
-      tab=[]
+      memory=[]
       for oid, val in varBinds:
-        tab.append(val.prettyPrint())
-
-    ram=float(tab[0])
-    ram_used=float(tab[1])
-    swap=float(tab[2])
-    swap_used=float(tab[2])
+        memory.append(val.prettyPrint())
   
-    return ram,ram_used,swap,swap_used
+    return memory
 
 def main():
 
-    host,port,username,auth_pass,priv_pass,warning,critical = parse_args()
+    args = parse_args()
    
-    ram,ram_used,swap,swap_used = get_data()
+    memory = data()
 
-    ram_used_percent=int((ram_used*100)/ram)
+    ram_used_percent = int((int(memory[1])*100)/int(memory[0]))
 
-    ram=int(ram/1024)
-    ram_used=int(ram_used/1024)
-    swap=swap/1024
-    swap_used=swap_used/1024
-   
-    if ram_used_percent >= critical:
+    swap_used_percent = int((int(memory[3])*100)/int(memory[2]))
+
+    if ram_used_percent >= args.critical:
       print ('CRITICAL - Memory usage %s%%' % (ram_used_percent))
       raise SystemExit(2)
-    elif ram_used_percent >= warning:  
+    elif ram_used_percent >= args.warning:  
       print ('WARNING - Memory usage %s%%' % (ram_used_percent))
       raise SystemExit(1)
     else:
