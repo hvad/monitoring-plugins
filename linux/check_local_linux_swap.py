e687b08618ab985b36068b5666ed25dcbda6e9bb
hvad <david.hannequin@gmail.com>
2020-09-27 23:12:08 +0200

    Pep8 fix for check_local_linux_swap.py.

--- linux/check_local_linux_swap.py
+++ linux/check_local_linux_swap.py
@@ -5,7 +5,7 @@
 #   Autors: David Hannequin <david.hannequin@gmail.com>,
 #   Date: 2017-02-17
 #   URL: https://github.com/hvad/monitoring-plugins
-#   
+#
 #   Plugins to check linux memory usage.
 #
 # Shinken plugin is free software: you can redistribute it and/or modify
@@ -21,21 +21,25 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Requires: Python >= 2.7 
+# Requires: Python >= 2.7
 # Requires: Psutil
 
 import argparse
 import psutil
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', '--warning', default='80', type=int, help='Warning thresold')
-    parser.add_argument('-c', '--critical', default='90', type=int, help='Critical thresold')
+    parser.add_argument('-w', '--warning', default='80',
+                        type=int, help='Warning thresold')
+    parser.add_argument('-c', '--critical', default='90',
+                        type=int, help='Critical thresold')
     args = parser.parse_args()
     warning = args.warning
     critical = args.critical
-    
-    return warning,critical
+
+    return warning, critical
+
 
 def bytes2human(n):
     symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
@@ -48,9 +52,9 @@ def bytes2human(n):
             return '%.1f%s' % (value, s)
     return "%sB" % n
 
+
 def get_data():
     swap_info = psutil.swap_memory()
-    #sswap(total=2147479552L, used=0L, free=2147479552L, percent=0.0, sin=0, sout=0)
     total = int(swap_info.total)
     used = int(swap_info.used)
     free = int(swap_info.free)
@@ -60,23 +64,34 @@ def get_data():
     used = bytes2human(used)
     free = bytes2human(free)
 
-    return total,used,free,percent
+    return total, used, free, percent
+
 
 def main():
 
-    warning,critical = parse_args()
+    warning, critical = parse_args()
 
-    total,used,free,percent = get_data()
+    total, used, free, percent = get_data()
 
     if percent >= critical:
-        print ('CRITICAL - Swap percentage usage : %2.1f%% Total Swap : %s Free Swap : %s Used Swap : %s |swap_percent =%s;%s;%s;0;100' % (percent, total, free, used, percent, warning, critical))
+        print('CRITICAL - Swap percentage usage : %2.1f%% Total Swap : \
+              %s Free Swap : %s Used Swap : %s \
+              |swap_percent =%s;%s;%s;0;100' % (percent, total,
+              free, used, percent, warning, critical))
         raise SystemExit(2)
     elif percent >= warning:
-        print ('WARNING - Swap percentage usage : %2.1f%% Total Swap : %s Free Swap : %s Used Swap : %s |swap_percent =%s;%s;%s;0;100' % (percent, total, free, used, percent, warning, critical))
+        print('WARNING - Swap percentage usage : %2.1f%% Total Swap : %s \
+              Free Swap : %s Used Swap : %s \
+              |swap_percent =%s;%s;%s;0;100' % (percent, total,
+              free, used, percent, warning, critical))
         raise SystemExit(1)
     else:
-        print ('OK - Swap percentage usage : %2.1f%% Total Swap : %s Free Swap : %s Used Swap : %s |swap_percent =%s;%s;%s;0;100' % (percent, total, free, used, percent, warning, critical))
+        print('OK - Swap percentage usage : %2.1f%% Total Swap : %s \
+              Free Swap : %s Used Swap : %s \
+              |swap_percent =%s;%s;%s;0;100' % (percent, total,
+              free, used, percent, warning, critical))
         raise SystemExit(0)
+
 
 if __name__ == "__main__":
     main()
