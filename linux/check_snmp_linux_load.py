50d54f79d66289ee06cbc0e331999e62dcc6ec57
hvad <david.hannequin@gmail.com>
2020-09-29 21:48:11 +0200

    Pep8 fix for check_snmp_linux_load.py.

--- linux/check_snmp_linux_load.py
+++ linux/check_snmp_linux_load.py
@@ -5,7 +5,7 @@
 #   Autors: David Hannequin <david.hannequin@gmail.com>,
 #   Date: 2019-08-08
 #   URL: https://github.com/hvad/monitoring-plugins
-#   
+#
 #   Plugins to check linux load by SNMP.
 #
 # Monitoring plugin is free software: you can redistribute it and/or modify
@@ -24,12 +24,15 @@
 from pysnmp.hlapi import *
 import argparse
 
+
 def parse_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-H', '--host', default='127.0.0.1', type=str, help='hostname or ip address')
+    parser.add_argument('-H', '--host', default='127.0.0.1',
+                        type=str, help='hostname or ip address')
     parser.add_argument('-p', '--port', default='161', type=int, help='port')
-    parser.add_argument('-C', '--community', default='public', type=str, help='snmp community')
+    parser.add_argument('-C', '--community', default='public',
+                        type=str, help='snmp community')
     parser.add_argument('-w', '--warning', default='3,2,1')
     parser.add_argument('-c', '--critical', default='4,3,2')
     args = parser.parse_args()
@@ -42,61 +45,69 @@ def parse_args():
 
     (cload1, cload5, cload15) = critical
     (wload1, wload5, wload15) = warning
-    
-    return host,port,community,cload1,cload5,cload15,wload1,wload5,wload15
+
+    return host, port, community, cload1, cload5, \
+        cload15, wload1, wload5, wload15
+
 
 def get_data():
 
-   host,port,community,cload1,cload5,cload15,wload1,wload5,wload15 = parse_args()
+    host, port, community, cload1, cload5, cload15, \
+        wload1, wload5, wload15 = parse_args()
 
-   errorIndication, errorStatus, errorIndex, varBinds = next(
-       getCmd(SnmpEngine(),
-              CommunityData(community),
-              UdpTransportTarget((host, port)),
-              ContextData(),
-              ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.1')),
-              ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.2')),
-              ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.3')))
-   )
-   
-   if errorIndication:
-       print(errorIndication)
-   elif errorStatus:
-       print('%s at %s' % (errorStatus.prettyPrint(),
-                           errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-   else:
-     tab=[]
-     for oid, val in varBinds:
-       tab.append(val.prettyPrint())
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        getCmd(SnmpEngine(),
+             CommunityData(community),
+             UdpTransportTarget((host, port)),
+             ContextData(),
+             ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.1')),
+             ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.2')),
+             ObjectType(ObjectIdentity('1.3.6.1.4.1.2021.10.1.3.3')))
+    )
 
-   load1=float(tab[0])
-   load5=float(tab[1])
-   load15=float(tab[2])
-   
-   load1=int(load1)
-   load5=int(load5)
-   load15=int(load15)
-   
-   return load1,load5,load15
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+              errorIndex and
+              varBinds[int(errorIndex) - 1][0] or '?'))
+    else:
+        tab = []
+        for oid, val in varBinds:
+            tab.append(val.prettyPrint())
+
+    load1 = float(tab[0])
+    load5 = float(tab[1])
+    load15 = float(tab[2])
+
+    load1 = int(load1)
+    load5 = int(load5)
+    load15 = int(load15)
+
+    return load1, load5, load15
+
 
 def main():
 
-    host,port,community,cload1,cload5,cload15,wload1,wload5,wload15 = parse_args()
-   
-    load1,load5,load15 = get_data()
+    host, port, community, cload1, cload5, cload15, \
+         wload1, wload5, wload15 = parse_args()
+
+    load1, load5, load15 = get_data()
 
     if load1 >= cload1 or load5 >= cload5 or load15 >= cload15:
-        print ('CRITICAL - Load average : %s,%s,%s|load1=%s;load5=%s;load15=%s'
-               % (load1, load5, load15, load1, load5, load15))
+        print('CRITICAL - Load average : %s,%s,%s \
+              |load1=%s;load5=%s;load15=%s'
+              % (load1, load5, load15, load1, load5, load15))
         raise SystemExit(2)
     elif load1 >= wload1 or load5 >= wload5 or load15 >= wload15:
-        print ('WARNING - Load average : %s,%s,%s|load1=%s;load5=%s;load15=%s'
-               % (load1, load5, load15, load1, load5, load15))
+        print('WARNING - Load average : %s,%s,%s|load1=%s;load5=%s;load15=%s'
+              % (load1, load5, load15, load1, load5, load15))
         raise SystemExit(1)
     else:
-        print ('OK - Load average : %s,%s,%s|load1=%s;load5=%s;load15=%s'
-               % (load1, load5, load15, load1, load5, load15))
+        print('OK - Load average : %s,%s,%s|load1=%s;load5=%s;load15=%s'
+              % (load1, load5, load15, load1, load5, load15))
         raise SystemExit(0)
-         
+
+
 if __name__ == "__main__":
     main()
